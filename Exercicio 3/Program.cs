2d00eb378be134b98b543176a63c8c2c36b8cab8
Leonardo Souza <leonardo030605@gmail.com>
2021-04-30 11:14:18 -0300

    correções no portugol

--- Exercicio 3/Program.cs
+++ Exercicio 3/Program.cs
@@ -12,10 +12,15 @@ namespace Exercicio_3
             Console.WriteLine("Favor digitar o valor a ser reajustado");
             salario = double.Parse(Console.ReadLine());
 
+            if(salario < 500){
             salario_novo = (salario * 30) / 100;
             salario = salario + salario_novo;
 
             Console.WriteLine("O seu novo salario é de: " + salario);
+            }else{
+                Console.WriteLine("Você não tem reajuste");
+            }
+
             Console.WriteLine("Fim do Programa!!");
         }
     }
