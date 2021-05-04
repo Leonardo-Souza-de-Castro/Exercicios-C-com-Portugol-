f9a6c2028330cfc4add65b5242d829031590bfd2
ViniciusVianazin <leonardo030605@gmail.com>
2021-05-04 11:01:41 -0300

    utilizando while no desafio

--- Desafio/Program.cs
+++ Desafio/Program.cs
@@ -6,38 +6,45 @@ namespace Desafio
     {
         static void Main(string[] args)
         {
-          string nome;
-          int idade;
+            string nome;
+            int idade;
+            bool resposta = false;
+            string sistema ="";
 
-          
-          Console.WriteLine("Ola!, Insira seu nome");
-          nome = Console.ReadLine();
-          Console.WriteLine("Olá " + nome + " qual a sua idade?");
-          idade = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Ola!, Insira seu nome");
+                nome = Console.ReadLine();
+                Console.WriteLine("Olá " + nome + " qual a sua idade?");
+                idade = int.Parse(Console.ReadLine());
 
-          if((idade >= 5) && (idade <= 7)){
-                Console.WriteLine("Você pertence a categoria Infantil A");
-          }
+                if ((idade >= 5) && (idade <= 7))
+                {
+                    Console.WriteLine("Você pertence a categoria Infantil A");
+                }
 
-           if ((idade >= 8) && (idade <= 10))
-           {
-                Console.WriteLine("Você pertence a categoria Infantil B");
-           }
+                if ((idade >= 8) && (idade <= 10))
+                {
+                    Console.WriteLine("Você pertence a categoria Infantil B");
+                }
 
-           if ((idade >= 11) && (idade <= 13))
-           {
-               Console.WriteLine("Você pertence a categoria Juvenil A");
-           }
+                if ((idade >= 11) && (idade <= 13))
+                {
+                    Console.WriteLine("Você pertence a categoria Juvenil A");
+                }
 
-           if ((idade >= 14) && (idade <=17))
-           {
-               Console.WriteLine("Você pertence a turma Juvenil B");
-           }
+                if ((idade >= 14) && (idade <= 17))
+                {
+                    Console.WriteLine("Você pertence a turma Juvenil B");
+                }
+                if ((idade < 5) || (idade > 17))
+                {
+                    Console.WriteLine("Percebemos que você não é de nenhuma turma \nGostaria de sair do sistema?");
+                    sistema = Console.ReadLine().ToLower();
+                }
 
-           if (idade >= 18)
-           {
-               Console.WriteLine("Você pertence a turma Sênior");
-           }
+            } while (sistema == "não");
+
         }
     }
 }
